f6edb4c169f389b1324d12ed98981fcde4cbd8fa
defendimattia <defendi.mattia92@gmail.com>
2025-03-11 17:37:28 +0100

    add: jss structure and cycle, working on imges

--- js/main.js
+++ js/main.js
@@ -36,3 +36,38 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+/*
+<div class="card">
+  <div class="card-img">
+      <img src="img/male1.png" alt="" id="img">
+  </div>
+  <div class="card-info">
+      <h3 id="name"></h3>
+      <span id="role"></span>
+      <span id="email"></span>
+  </div>
+</div>
+*/
+
+
+const gridContainerElement = document.querySelector(".grid-container")
+
+for (i = 0; i < teamMembers.length; i++) {
+  const member = teamMembers[i]
+  const toRender = `<div class="card">
+    <div class="card-img">
+        <img src="img/male1.png" alt="" id="img">
+    </div>
+    <div class="card-info">
+        <h3 id="name">${member.name}</h3>
+        <span id="role">${member.role}</span>
+        <a href="#" id="email">${member.email}</a>
+    </div>
+  </div>`
+
+  gridContainerElement.innerHTML += toRender
+}
+
+
